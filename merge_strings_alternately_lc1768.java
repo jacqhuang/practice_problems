00ed05ebfabef89f4fd218c7efae1dd9dc751751
jacqhuang <jhuan172@asu.edu>
2024-01-22 20:04:40 -0600

    completed leetcode 1768

---
+++ merge_strings_alternately_lc1768.java
@@ -0,0 +1,30 @@
+class Solution {
+	public String mergeAlternately(String word1, String word2) {
+		int i = 0;
+		int j = 0;
+		StringBuilder str = new StringBuilder();
+
+		while(i < word1.length() && j < word2.length()) {
+			str.append(word1.charAt(i));
+			str.append(word2.charAt(j));
+			i++;
+			j++;
+		}
+
+		while(i < word1.length()) {
+			str.append(word1.charAt(i));
+			i++;
+		}
+
+		while(j < word2.length()) {
+			str.append(word2.charAt(j));
+			j++;
+		}
+		return str.toString();
+	}
+}
+
+/*
+	Time Complexity: O(n)
+	Space Complexity: O(n)
+*/
